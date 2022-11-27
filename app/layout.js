44c769e2f1cc94d35b269f80a75867dac198cf34
Chinmay Sa <68277286+ScreenTechnicals@users.noreply.github.com>
2022-11-27 16:32:43 +0530

    Initial Commit4

--- app/layout.js
+++ app/layout.js
@@ -53,7 +53,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
       <title></title>
-        <meta name="viewport" content="width=device-width, initial-scale=1"  />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"  /> */}
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body className="w-screen overflow-hidden flex">
